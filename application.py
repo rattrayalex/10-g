cddd96fe3830bebeeb9721ef3c7fb9d5d09bdb36
Alex Rattray <rattray.alex@gmail.com>
2012-07-21 21:29:52 -0700

    penncycle api, better loading of categories

--- application.py
+++ application.py
@@ -3,7 +3,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+  statements = [
+    {'name':'Balance Sheet', 'slug':'balance_sheet', 'fields':[
+      'Total Assets', 
+      'Total Liabilities',
+      'Total Shareholder\'s Equity',
+    ]}, 
+    {'name':'Statement of Cash Flows', 'slug':'cash_flows', 'fields':[
+      'Net Income',
+      'Net Cash from Operations',
+      'Net Cash from Financing',
+      'Net Cash used in Investing',
+      'Net change in Cash',
+    ]},
+    {'name':'Income Statement', 'slug':'income_statement', 'fields':[
+      'Total Revenue',
+      'Net Income',
+      'SG&A',
+      'Research and Development',
+      'Cost of Revenue',
+      'Total Operating Expenses',
+      'Earnings Per Share (Diluted)',
+      'Earnings Per Share (Basic)',
+    ]},
+  ]
+  return render_template('index.html', statements=statements)
 
 if __name__ == '__main__':
   app.debug = True
