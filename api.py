ad6cda79a25bec8142ed345a76b0e1248d3cad0e
Zachary Wasserman <zachwass2000@gmail.com>
2012-07-22 11:47:41 -0700

    Added data interpolation

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import psycopg2
+from itertools import cycle
 from pymongo import Connection
 from collections import defaultdict
 
@@ -18,7 +19,7 @@ CostOfGoodsSold
 Debt
 Depreciation #
 DepreciationAndAmortization
-EarningsPerShare 
+EarningsPerShare
 GrossProfit
 IncomeTaxes #
 Inventories
@@ -37,7 +38,7 @@ StockholdersEquity
 """
 
 
-connection = Connection()
+connection = Connection("data.10-g.com", 27017)
 
 
 def get_companies_dict(infile="cik_ticker.txt"):
@@ -83,26 +84,53 @@ ALL_INFO = [ "CashAtCarryingValue",
 
 
 def get_listings(ciks, company_info = ALL_INFO):
+    """Lookup the listings for the given companies
+
+    Example cik: 0000789019 - msft
+    0000320193 - aapl
+    0001288776 - goog
+    """
+
+
     db = connection.sec_data
     companies = db.companies
     output = []
+    inner_output = []
     for cik in ciks:
         company = companies.find_one({'cik' : cik})
-	temp_dic = defaultdict(dict)
+        temp_dic = defaultdict(dict)
         for key, values in company['values'].items():
             for value in values:
                 if len(value['period']) == 2 and value['period'][1] == 'Q':
-	            temp_dic[str(value['year']) + value['period'][::-1]][key] = value
-        for time, info in temp_dic.items():
+                    temp_dic[str(value['year']) + value['period'][::-1]][key] = value
+        for time, info in sorted(temp_dic.items()):
+            #sorted in chronological order
             print info
             line = [company['name'], time]
             for values in company_info:
                 if values in info:
                     line.append(info[values]['value'])
                 else:
-                    line.append(0)
+                    line.append(None)
             output.append(line)
-    return output
+
+        transpose = map(list, zip(*[x[2:] for x in output]))
+        for param in transpose:
+            prev_val = 0
+            try:
+                prev_val = next(s for s in param if s)
+            except:
+                pass
+            #enumerate over the values of this parameter over time
+            for index, value in enumerate(param):
+                if value is not None:
+                    param[index] = value
+                    prev_val = value
+                else:
+                    param[index] = prev_val
+        detransposed = zip(*transpose)
+        inner_output.extend([zipped[0][:2] + list(zipped[1]) for zipped in zip(output, detransposed)])
+    return inner_output
 
 def get_ciks_for_group(x):
     divisions = connection.sec_data2.devisions
@@ -118,4 +146,3 @@ def get_ciks_for_group(x):
         pass
     elif len(x) == 4:
         pass
-        
